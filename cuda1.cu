27540eb113cb65a60619050fc8182c55e1314e2d
paola-md <paola.domen@gmail.com>
2019-05-06 09:44:01 -0500

    e

--- cuda1.cu
+++ cuda1.cu
@@ -54,7 +54,7 @@ int main(int argc, char* argv[]) {
 
 	int blockSize = m*m;
 	//mult<<<numBlocks, blockSize>>>(d_a, d_b, d_c, m);
-	mult<<<1, blockSize>>>(d_a, d_b, d_c, m);
+	mult<<<blockSize, blockSize>>>(d_a, d_b, d_c, m);
     
      // Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();	
