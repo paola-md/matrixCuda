880f051836729aa18aaf927b62993d818e34a3ad
paola-md <paola.domen@gmail.com>
2019-05-06 09:47:21 -0500

    e

--- cuda1.cu
+++ cuda1.cu
@@ -8,6 +8,7 @@ void mult(int *d_a,int *d_b, int *d_c, int m)
     int row = blockIdx.y * blockDim.y + threadIdx.y; 
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
+	printf("Hola, entre row = %d, col = %d", row, col);
     if( col < m && row < m) 
     {
         for(int i = 0; i < m; i++) 
