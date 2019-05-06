0c51eb1c5a6302a6ea7ca9f81376c47ad551d8d5
paola-md <paola.domen@gmail.com>
2019-05-06 09:36:49 -0500

    e

---
+++ cuda1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+__global__ 
+void mult(int *d_a,int *d_b, int *d_c, int m)
+{ 
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int sum = 0;
+    if( col < m && row < m) 
+    {
+        for(int i = 0; i < m; i++) 
+        {
+            sum += d_a[row * m + i] * d_b[i * m + col];
+        }
+        d_c[row * m + col] = sum;
+    }
+} 
+
+
+
+int main(int argc, char* argv[]) {
+	
+	int m;
+    printf("Ingresa el tamaño de la matrix cuadrada \n");
+    scanf("%d", &m);
+	 int blockSize = 256;
+	int numBlocks = (N + blockSize - 1) / blockSize;
+	
+	// Allocate memory space on the device 
+	  int *d_a, *d_b, *d_c;
+	  // Allocate Unified Memory – accessible from CPU or GPU
+	  cudaMallocManaged(&d_a, sizeof(int)*m*m);
+	  cudaMallocManaged(&d_b, sizeof(int)*m*m); 
+	  cudaMallocManaged(&d_c, sizeof(int)*m*m); 
+		
+	
+	int i, j;
+    //initialize matrix A
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+            d_a[i * m + j] = 1; 
+        }
+    }
+
+    //initialize matrix B
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+            d_b[i * m + j] = 2; 
+        }
+    }
+
+
+
+	mult<<<numBlocks, blockSize>>>(d_a, d_b, d_c, m);
+
+    
+     // Wait for GPU to finish before accessing on host
+	cudaDeviceSynchronize();	
+
+
+	printf("\n Resultado \n");
+	/* Check for errors (all values should be 3.0f) */ 
+	int res = m*2;
+	int maxError = 0;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+			maxError = fmax(maxError, fabs( h_c[i * m + j]-res));
+            printf("%d ", d_c[i * m + j] ); 
+        }
+		printf("\n");
+    }
+	
+	  printf("Max error: %d \n", maxError);
+	
+	
+	// free memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+	return 0;
+}
--- matrix.c
+++ matrix.c
@@ -1,14 +1,62 @@
-void cpu_matrix_mult(int *h_a, int *h_b, int *h_result, int m, int n, int k) {
-    for (int i = 0; i < m; ++i) 
+#include <stdio.h>
+#include <stdlib.h>
+
+void mult(int *h_a, int *h_b, int *h_c, int m) {
+    int i;
+	int j;
+	int h;
+	for (i = 0; i < m; ++i) 
     {
-        for (int j = 0; j < k; ++j) 
+        for (j = 0; j < m; ++j) 
         {
             int tmp = 0.0;
-            for (int h = 0; h < n; ++h) 
+            for ( h = 0; h < m; ++h) 
             {
-                tmp += h_a[i * n + h] * h_b[h * k + j];
+                tmp += h_a[i * m + h] * h_b[h * m + j];
             }
-            h_result[i * k + j] = tmp;
+            h_c[i * m+ j] = tmp;
         }
     }
+}
+
+
+int main(int argc, char* argv[]) {
+	
+	int m;
+    printf("Ingresa el tamaño de la matrix cuadrada \n");
+    scanf("%d", &m);
+	
+
+    int *h_a = (int *)malloc(sizeof(int)*m*m);
+	int *h_b = (int *)malloc(sizeof(int)*m*m);
+	int *h_c = (int *)malloc(sizeof(int)*m*m);
+	
+	int i, j;
+    //initialize matrix A
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+            h_a[i * m + j] = 1; 
+        }
+    }
+
+    //initialize matrix B
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+            h_b[i * m + j] = 2; 
+        }
+    }
+
+
+    mult(h_a, h_b, h_c, m);   
+    
+	printf("\n Resultado \n");
+	// print matrix
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < m; ++j) {
+            printf("%d ", h_c[i * m + j] ); 
+        }
+		printf("\n");
+    }
+
+	return 0;
 }
